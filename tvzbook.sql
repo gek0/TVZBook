803540f2a3c610941ef33bf5894346bfd529281d
Matija Buriša <matijaburisa@gmail.com>
2016-12-25 01:58:25 +0100

    added database structure
    added database structure as sql file

---
+++ tvzbook.sql
@@ -0,0 +1,79 @@
+/*
+SQLyog Ultimate v9.51 
+MySQL - 5.6.24 : Database - tvzbook
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tvzbook` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `tvzbook`;
+
+/*Table structure for table `comments` */
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `author_id` int(10) NOT NULL,
+  `post_id` int(10) NOT NULL,
+  `comment_text` varchar(255) NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `likes` */
+
+DROP TABLE IF EXISTS `likes`;
+
+CREATE TABLE `likes` (
+  `post_id` int(10) NOT NULL,
+  `users_list` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `posts` */
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `author_id` int(10) NOT NULL,
+  `post_text` text,
+  `status` enum('public','private') NOT NULL DEFAULT 'public',
+  `comment_number` int(10) NOT NULL DEFAULT '0',
+  `like_number` int(10) NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `phone_number` varchar(60) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `ip` varchar(60) NOT NULL,
+  `registration_date` datetime NOT NULL,
+  `last_online` datetime NOT NULL,
+  `avatar` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_unique` (`username`,`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
